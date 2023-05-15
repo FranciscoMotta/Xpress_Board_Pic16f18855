1b57fd5917f23b52c59295eb84620eb742e3d289
Julio Francisco Motta Claudio <59737000+FranciscoMotta@users.noreply.github.com>
2023-05-14 21:25:11 -0500

    Add gpio lcd codes

--- micro_00_gpio/lcd_4_bits.X/main.c
+++ micro_00_gpio/lcd_4_bits.X/main.c
@@ -33,20 +33,35 @@
 
 #define Lcd_Ctrl_Tris   TRISC
 #define Lcd_Ctrl_Lat    LATC
-#define Lcd_Ctrl_Ansel
+#define Lcd_Ctrl_Ansel  ANSELC
 #define Lcd_Ctrl_Rs     0
 #define Lcd_Ctrl_Rw     1
 #define Lcd_Ctrl_En     2
 #define Lcd_Ctrl_Mask  (1 << Lcd_Ctrl_En) | (1 << Lcd_Ctrl_Rw) | (1 << Lcd_Ctrl_Rs)
 
+/* Rutinas del enable */
+
+#define En_On()         Lcd_Ctrl_Lat |= (1 << Lcd_Ctrl_En);
+#define En_Off()        Lcd_Ctrl_Lat &= ~(1 << Lcd_Ctrl_En);
+#define Lcd_En_Tog()    En_On(); __delay_ms(1); En_Off(); __delay_ms(1);
+
+/* Rutina de modos */
+
+#define Lcd_Mode_Com()  Lcd_Ctrl_Lat &= ~(1 << Lcd_Ctrl_Rs);
+#define Lcd_Mode_Char() Lcd_Ctrl_Lat |= (1 << Lcd_Ctrl_Rs);
 
 /* Declaracion de funciones */
 
 void Init_HFINTOSC (void);
+void Init_System_Gpio (void);
 
 /* LCD */
 
 void FM_Lcd_Init_Gpio (void);
+void FM_Lcd_Init_Config (void);
+void FM_Lcd_Send_Nibble(char nibble); 
+void FM_Lcd_Send_Command (char comm);
+void FM_Lcd_Send_Charact (char charac);
 
 /*
  * MAIN
@@ -55,31 +70,86 @@ void FM_Lcd_Init_Gpio (void);
 int main(void) 
 {
     Init_HFINTOSC(); // Conf. Reloj Interno
-    
-    /* Led como salida */
-    Led_Sys_Ansel &= ~(1 << Led_Sys_Gpio);
-    Led_Sys_Tris &= ~(1 << Led_Sys_Gpio);
-    
+    Init_System_Gpio(); // Iniciamos el GPIO
+    FM_Lcd_Init_Config(); // Iniciamos el LCD
+    FM_Lcd_Send_Command(0x80);
+    FM_Lcd_Send_Charact('a');
     while (true)
     {
         Led_Sys_Lat ^= (1 << Led_Sys_Gpio);
-        __delay_ms(100);
+        __delay_ms(200);
     }
     return (EXIT_SUCCESS);
 }
 
 /* Definicion de funciones */
 
+
+void Init_System_Gpio (void)
+{
+    Led_Sys_Ansel &= ~(1 << Led_Sys_Gpio);
+    Led_Sys_Tris &= ~(1 << Led_Sys_Gpio);
+}
+
+void FM_Lcd_Init_Config (void)
+{
+    FM_Lcd_Init_Gpio();// Iniciamos los GPIO para la LCD
+    /* Acá procedemos a mandar los comandos de control */
+    FM_Lcd_Send_Command(0x01); // Clear Display
+    FM_Lcd_Send_Command(0x02); // Return Home
+    FM_Lcd_Send_Command(0x06); // Cursor to right
+    FM_Lcd_Send_Command(0x0C); // Display On - Cursor Off - Blink Off
+    FM_Lcd_Send_Command(0x10); // Shift cursor to right AC + 1
+    FM_Lcd_Send_Command(0x2C); // 4bits LCD - 2 Lines - 5x11 dots
+}
+
+
+void FM_Lcd_Send_Charact (char charac)
+{
+    Lcd_Mode_Char();
+    FM_Lcd_Send_Nibble(charac);
+}
+
+void FM_Lcd_Send_Command (char comm)
+{
+    Lcd_Mode_Com();
+    FM_Lcd_Send_Nibble(comm);
+}
+
+void FM_Lcd_Send_Nibble(char nibble)
+{
+    char upper_bits,lower_bits = 0;
+    upper_bits = (nibble & 0xF0);
+    lower_bits = (nibble & 0x0F) << 4;
+    Lcd_Data_Lat |= upper_bits;
+    Lcd_En_Tog();
+    Lcd_Data_Lat &= ~upper_bits;
+    
+    __delay_us(100); // Esperamos 0.01ms
+    
+    Lcd_Data_Lat |= lower_bits;
+    Lcd_En_Tog();
+    Lcd_Data_Lat &= ~lower_bits;
+    
+    __delay_us(100); // Esperamos un 0.01ms 
+}
+
 void FM_Lcd_Init_Gpio (void)
 {
     /* Pines de datos digitales */
     Lcd_Data_Ansel &= ~Lcd_Data_Mask;
     /* Pines de control digitales */
-    
+    Lcd_Ctrl_Ansel &= ~Lcd_Ctrl_Mask;
     /* Pines de datos como salida: RC7 - RC6 - RC5 - RC4 */
     Lcd_Data_Tris &= ~Lcd_Data_Mask;
     /* Pines de control como salida: RC2 - RC1 - RC0 */
     Lcd_Data_Tris &= ~Lcd_Ctrl_Mask;
+    /* Todos los pines en 0 */
+    Lcd_Data_Lat &= ~Lcd_Data_Mask;
+    Lcd_Ctrl_Lat &= ~Lcd_Ctrl_Mask;
+    
+    /* Ponemos el RW en 0 */
+    Lcd_Ctrl_Lat &= ~(1 << Lcd_Ctrl_Rw);
 }
 
 void Init_HFINTOSC (void)
